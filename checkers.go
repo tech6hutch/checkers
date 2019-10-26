768f7e60993ec537dd13bc2f7be950754c33411a
Hutch <tech_hutch@verizon.net>
2019-10-26 08:44:30 -0400

    started player turns loop

--- checkers.go
+++ checkers.go
@@ -1,13 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/tech6hutch/checkers/model/board"
+	"github.com/tech6hutch/checkers/model/pieces"
+	"os"
 )
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Println("Checkers!")
 	board := board.New()
+	playerColors := &map[int]pieces.Color{
+		1: pieces.ColorDark,
+		2: pieces.ColorLight,
+	}
+	playerTurn := 1
+
+	for {
+		switch playerTurn {
+		case 1: playerTurn = 2
+		case 2: playerTurn = 1
+		default: panic("wtf")
+		}
+
+		fmt.Printf("Player %d's turn! (%s)\n",
+			playerTurn, (*playerColors)[playerTurn])
+		fmt.Println(board)
+
+		scanner.Scan()
+		line := scanner.Text()
+		fmt.Println(line)
+	}
 
 	fmt.Println(board)
 }
--- model/pieces/pieces.go
+++ model/pieces/pieces.go
@@ -7,6 +7,14 @@ const (
 	ColorLight
 )
 
+func (color Color) String() string {
+	switch color {
+	case ColorDark: return "dark"
+	case ColorLight: return "light"
+	default: panic("unknown variant")
+	}
+}
+
 type Kind int
 
 const (
