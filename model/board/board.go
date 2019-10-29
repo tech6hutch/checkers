346b1aac5cfe46bb35759a0cf8e824feaad92fcb
Hutch <tech_hutch@verizon.net>
2019-10-29 16:26:42 -0400

    started adding row/column markers

--- model/board/board.go
+++ model/board/board.go
@@ -2,7 +2,9 @@ package board
 
 import (
 	"github.com/tech6hutch/checkers/model/pieces"
+	"strconv"
 	"strings"
+	"unicode/utf8"
 )
 
 const Width = 8
@@ -37,7 +39,7 @@ func (board *Board) initRow(rowN int, color pieces.Color) {
 func (board *Board) verifyPiecePositions() {
 	for rowN, row := range board {
 		for colN, p := range row {
-			if rowN % 2 == colN % 2 && p != nil {
+			if rowN%2 == colN%2 && p != nil {
 				panic("wrong piece placement")
 			}
 		}
@@ -46,13 +48,43 @@ func (board *Board) verifyPiecePositions() {
 
 func (board *Board) String() string {
 	var b strings.Builder
-	for _, row := range board {
+
+	for colN := 0; colN < Width; colN++ {
+		n := string(colN)
+		b.WriteString(n)
+	}
+	b.WriteRune('\n')
+
+	for rowN, row := range board {
+		// todo: instead, add to number first, to get letter
+		rowLetter, _ := utf8.DecodeRuneInString(strconv.Itoa(rowN))
+		// b.WriteRune(rowLetter)
+		b.WriteString(rowLetter)
+		for colN := range row {
+			numStr := strconv.Itoa(colN)
+			numRune, _ := utf8.DecodeRuneInString(numStr)
+			b.WriteRune(numRune)
+		}
+	}
+	for rowN, rowLetter := 0, 65; rowN < Width; rowN, rowLetter = rowN+1, rowLetter+1 {
+		row := board[rowN]
+		for colN, p := range row {
+
+		}
+	}
+	for rowN, row := range board {
+		rowLetter := rune(rowN + 65)
+		b.WriteRune(rowLetter)
 		for _, p := range row {
 			switch {
-			case p == nil:              b.WriteRune('#')
-			case p.Kind == pieces.Man:  b.WriteRune('*')
-			case p.Kind == pieces.King: b.WriteRune('^')
-			default: panic("unknown piece kind")
+			case p == nil:
+				b.WriteRune('#')
+			case p.Kind == pieces.Man:
+				b.WriteRune('*')
+			case p.Kind == pieces.King:
+				b.WriteRune('^')
+			default:
+				panic("unknown piece kind")
 			}
 		}
 		b.WriteRune('\n')
